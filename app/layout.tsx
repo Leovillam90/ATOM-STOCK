fe35cda769e11fa9d8f66668c8c12301411e0b81
Leovillam90 <Info@atomsolutionsdata.com>
2026-08-19 13:36:42 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -334,7 +334,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       path: '/traslados',
       disabled: false,
       badge: null,
-      rolesPermitidos: ['ADMIN', 'GERENTE_BODEGA'],
+      rolesPermitidos: ['ADMIN', 'GERENTE_BODEGA','VENDEDOR'],
       icon: (
         <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4" />
