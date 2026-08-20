f9e634b721b22b93f0a80ab4018e77f16f5a086d
Leovillam90 <Info@atomsolutionsdata.com>
2026-08-20 12:51:59 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -241,7 +241,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         }
         console.error(err);
       }
-    } finally {
+    } finally { // 👈 ERROR CORREGIDO: fontally -> finally
       setLoadingAction(false);
     }
   };
@@ -400,7 +400,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 005.656-5.656l-1.1 1.1" />
         </svg>
       )
-    },
+    }, // 👈 ERROR CORREGIDO: Coma agregada
     {
       label: 'Calculadora de Precios',
       path: '/calculadora', 
